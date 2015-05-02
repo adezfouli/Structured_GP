f9d7965d138577b702d0c29766e58b8efd686f56
adezfouli <akdezfuli@gmail.com>
2015-05-03 01:10:23 +1000

    .

--- GP/experiment_run.py
+++ GP/experiment_run.py
@@ -29,8 +29,8 @@ class ExperimentRunner:
     @staticmethod
     def get_experiments():
         # return [Experiments.boston_data.__name__]
-        # return [Experiments.wisconsin_breast_cancer_data.__name__]
-        return [Experiments.USPS_data.__name__]
+        return [Experiments.wisconsin_breast_cancer_data.__name__]
+        # return [Experiments.USPS_data.__name__]
 
     @staticmethod
     def get_log_level():
