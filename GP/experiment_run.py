47616ff2ef51c863d1e458c3363f3c2a14c5aeaf
adezfouli <akdezfuli@gmail.com>
2015-05-17 20:11:00 +1000

    m

--- GP/experiment_run.py
+++ GP/experiment_run.py
@@ -13,9 +13,9 @@ class ExperimentRunner:
 
         configs = []
         expr_names = ExperimentRunner.get_experiments()
-        methods = ['mix2']
-        # sparse_factor = [1.0, 0.8, 0.6]
-        sparse_factor = [0.4, 0.2, 0.1]
+        methods = ['full', 'mix1', 'mix2']
+        sparse_factor = [1.0, 0.8, 0.6, 0.4, 0.2, 0.1]
+        # sparse_factor = [0.4, 0.2, 0.1]
         run_ids = [1, 2, 3, 4, 5]
         for e in expr_names:
             for m in methods:
@@ -37,9 +37,9 @@ class ExperimentRunner:
         """
 
         # uncomment to run desired experiment
-        # return [Experiments.boston_data.__name__]
+        return [Experiments.boston_data.__name__]
         # return [Experiments.wisconsin_breast_cancer_data.__name__]
-        return [Experiments.USPS_data.__name__]
+        # return [Experiments.USPS_data.__name__]
         # return [Experiments.creep_data.__name__]
         # return [Experiments.abalone_data.__name__]
 
