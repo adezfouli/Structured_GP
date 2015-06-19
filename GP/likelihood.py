corrected gradients

GP/likelihood.py
@@ -4,7 +4,7 @@ from scipy.linalg import inv, det
 from scipy.misc import logsumexp
 from scipy.special import erfinv
 from scipy.special._ufuncs import gammaln
-from GPstruct.prepare_from_data_chain import log_likelihood_function_numba
+from GPstruct.prepare_from_data_chain import log_likelihood_function_numba, marginals_function
 
 from util import cross_ent_normal, drange
 
@@ -414,7 +414,6 @@ class StructLL(Likelihood):
             .reshape((self.dim, self.n_samples))
         self.bin_dim = self.dataset.n_labels ** 2
 
-
     def ll_F_Y(self, F, Y, model):
 
         ll = np.empty((F.shape[0], self.dataset.object_size.sum()))
@@ -429,7 +428,6 @@ class StructLL(Likelihood):
 
         return ll, None, total_ll / F.shape[0]
 
-
     def set_params(self, p):
         if p.shape[0] != 0:
             raise Exception("struct ll function does not have free parameters")
@@ -440,10 +438,11 @@ class StructLL(Likelihood):
     def get_num_params(self):
         return 0
 
-    def predict(self, mu, sigma, Ys, model=None):
+    def predict(self, mu, chol_sigma, Ys, model=None):
         F = np.empty((self.n_samples, mu.shape[0], self.dim))
         for j in range(self.dim):
-            F[:, :, j] = np.outer(self.normal_samples[j, :], np.sqrt(sigma[:, j])) + mu[:, j]
+            F[:, :, j] = mdot(norm_samples, chol_sigma[j])
+            F[:, :, j] = F[:, :, j] + mu[j]
 
         Ys= np.empty((self.n_samples, mu.shape[0], self.test_dataset.n_labels))
         for s in range(F.shape[0]):

GP/savigp_test.py
@@ -1,30 +1,25 @@
+__author__ = 'AT'
+
 import logging
-from numpy.random import random
-from scipy.stats import uniform
+
+from structured_gp import StructureGP
+from gpstruct_wrapper import gpstruct_wrapper
+from ExtRBF import ExtRBF
 from data_source import DataSource
-from data_transformation import MeanTransformation, IdentityTransformation
+from data_transformation import IdentityTransformation
 from experiments import Experiments
 from plot_results import PlotOutput
 from savigp_diag import SAVIGP_Diag
 from savigp_single_comp import SAVIGP_SingleComponent
-
-__author__ = 'AT'
-
-from copy import deepcopy, copy
-import warnings
-from numpy.ma import trace
-from scipy.linalg import inv, det
-from sklearn import preprocessing
+from copy import deepcopy
 import GPy
 from matplotlib.pyplot import show
-from GPy.util.linalg import mdot
-import numpy as np
 from optimizer import *
 from savigp import Configuration
-from likelihood import UnivariateGaussian, MultivariateGaussian
+from likelihood import UnivariateGaussian, MultivariateGaussian, StructLL
 from grad_checker import GradChecker
 from plot import plot_fit
-from util import chol_grad, jitchol, bcolors
+from util import bcolors
 
 
 class SAVIGP_Test:
@@ -176,6 +171,66 @@ class SAVIGP_Test:
 
 
     @staticmethod
+    def test_struct():
+        np.random.seed(120)
+
+        configs = [
+            [
+                Configuration.MoG,
+                # Configuration.ENTROPY,
+                # Configuration.CROSS,
+                Configuration.ELL,
+            ],
+            [
+                Configuration.MoG,
+                Configuration.ENTROPY,
+            ],
+            [
+                Configuration.MoG,
+                Configuration.CROSS,
+            ],
+        ]
+
+
+        (ll_train,
+        posterior_marginals_test,
+        compute_error_nlm,
+        ll_test,
+        average_marginals,
+        write_marginals,
+        read_marginals,
+        n_labels,
+        Xtrain,
+        Xtest,
+         train_dataset,
+         test_dataset) = gpstruct_wrapper()
+
+        Xtrain = np.array(Xtrain.todense())
+
+        num_latent_proc = n_labels
+        kernel = [ExtRBF(Xtrain.shape[1], variance=1, lengthscale=np.array((1.,)), ARD=False) for j in range(num_latent_proc)]
+        # number of inducing points
+        num_inducing = int(Xtrain.shape[0] * 0.2)
+        num_samples = 20000
+        cond_ll = StructLL(ll_train, train_dataset, test_dataset)
+
+        s1 = StructureGP(Xtrain, np.empty((Xtrain.shape[0], 1)), num_inducing, cond_ll,
+                                      kernel, num_samples, configs[0], 0, False, True)
+
+        s1.rand_init_mog()
+
+        def f(x):
+            s1.set_params(x)
+            return s1.objective_function()
+
+        def f_grad(x):
+            s1.set_params(x)
+            return s1.objective_function_gradients()
+
+        return GradChecker.check(f, f_grad, s1.get_params(), s1.get_param_names(), verbose=True)
+
+
+    @staticmethod
     def init_test():
         np.random.seed(12000)
         num_input_samples = 3
@@ -303,5 +358,6 @@ class SAVIGP_Test:
 if __name__ == '__main__':
     # SAVIGP_Test.test_gp(True, method='full')
     # SAVIGP_Test.init_test()
-    SAVIGP_Test.test_grad()
+    # SAVIGP_Test.test_grad()
+    SAVIGP_Test.test_struct()
     # SAVIGP_Test.test_savigp({'method': 'full', 'sparse_factor': 1.0})

GP/structured_gp.py
@@ -1,15 +1,15 @@
+from scipy.misc import logsumexp
+
+__author__ = 'AT'
+
 from GPy.util.linalg import mdot
 
 from scipy.linalg import block_diag, inv
 import numpy as np
 from savigp import Configuration
 
-from mog_diag import MoG_Diag
-
 from util import jitchol, inv_chol
 from savigp_single_comp import SAVIGP_SingleComponent
-
-__author__ = 'AT'
 
 
 class StructureGP(SAVIGP_SingleComponent):
@@ -30,12 +30,16 @@ class StructureGP(SAVIGP_SingleComponent):
                                           kernels, n_samples, config_list, latent_noise,
                                           is_exact_ell, inducing_on_Xs, n_threads, image, partition_size)
 
+    def rand_init_mog(self):
+        super(StructureGP, self).rand_init_mog()
+        self.bin_m = np.random.uniform(low=1., high=2., size=self.bin_m.shape[0])
+        self.bin_s = np.random.uniform(low=1., high=3., size=self.bin_s.shape[0])
+
     def _sigma(self, k, j, Kj, Aj, Kzx):
         """
         calculating [sigma_k(n)]j,j for latent process j (eq 20) for all k
         """
         return Kj + self.MoG.aSkja_full(Aj, k, j)
-
 
     def _chol_sigma(self, sigma):
         sigmas = [0] * (len(self.seq_poses) - 1)
@@ -80,17 +84,14 @@ class StructureGP(SAVIGP_SingleComponent):
         self._bin_KL()
         super(StructureGP, self)._update()
         self.ll += self.bin_ent + self.bin_NCE
-        if Configuration.MoG in self.config_list:
-            self.grad_ll = np.hstack((self.grad_ll, self.bin_dM, self.bin_dL * self.bin_s))
-
+        self.grad_ll = np.hstack((self.grad_ll, self.bin_dM, self.bin_dL * self.bin_s))
 
     def get_binary_sample(self):
-        return  (self.binary_normal_samples.T * np.sqrt(self.bin_s) + self.bin_m)
+        return self.binary_normal_samples.T * np.sqrt(self.bin_s) + self.bin_m
 
     def get_all_params(self):
         parent_params = super(StructureGP, self).get_all_params()
         return np.hstack((parent_params, self.bin_m.copy(), np.log(self.bin_s.copy())))
-
 
     def get_params(self):
         parent_params = super(StructureGP, self).get_params()
@@ -98,6 +99,37 @@ class StructureGP(SAVIGP_SingleComponent):
 
     def set_params(self, p):
         self.bin_s = np.exp(p[-self.bin_m.shape[0]:])
-        self.bin_m = p[-(self.bin_m.shape[0]+self.bin_m.shape[0]):-self.bin_m.shape[0]]
+        self.bin_m = p[-(self.bin_m.shape[0]+self.bin_s.shape[0]):-self.bin_m.shape[0]]
+        super(StructureGP, self).set_params(p[:-(self.bin_m.shape[0]+self.bin_s.shape[0])])
         self._update()
 
+    def get_param_names(self):
+        parent_names = super(StructureGP, self).get_param_names()
+        return parent_names + ['b_m'] * self.cond_likelihood.bin_dim + ['b_s'] * self.cond_likelihood.bin_dim
+
+    def _predict_comp(self, Xs, Ys):
+        A, Kzx, K = self._get_A_K(Xs)
+
+        predicted_mu = np.empty((Xs.shape[0], self.num_mog_comp, self.cond_likelihood.output_dim()))
+        predicted_var = np.empty((Xs.shape[0], self.num_mog_comp, self.cond_likelihood.output_dim()))
+        nlpd = None
+        if not (Ys is None):
+            nlpd = np.empty((Xs.shape[0], self.num_mog_comp))
+
+        mean_kj = np.empty((self.num_mog_comp, self.num_latent_proc, Xs.shape[0]))
+        sigma_kj = np.empty((self.num_mog_comp, self.num_latent_proc, Xs.shape[0]))
+        chol_sigma = np.empty((self.num_mog_comp, self.num_latent_proc, Xs.shape[0], Xs.shape[0]))
+        for k in range(self.num_mog_comp):
+            for j in range(self.num_latent_proc):
+                mean_kj[k,j] = self._b(k, j, A[j], Kzx[j].T)
+                sigma_kj[k,j] = self._sigma(k, j, K[j], A[j], Kzx[j].T)
+                chol_sigma[k,j], _, _ = self._chol_sigma(sigma_kj[k,j])
+
+            if not (Ys is None):
+                predicted_mu[:, k, :], predicted_var[:, k, :], nlpd[:, k] = \
+                    self.cond_likelihood.predict(mean_kj[k, :].T, chol_sigma[k, :].T, Ys, self)
+            else:
+                predicted_mu[:, k, :], predicted_var[:, k, :], _ = \
+                    self.cond_likelihood.predict(mean_kj[k, :].T, sigma_kj[k, :].T, Ys, self)
+
+        return predicted_mu, predicted_var, -logsumexp(nlpd, 1, self.MoG.pi)